156de0bc175a5e3a75aa1ce4c056b711b8aa668b
adairrohling <81537071+adairrohling@users.noreply.github.com>
2024-06-12 01:05:35 +0000

    fix: layout final

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,11 +6,8 @@ import { MenuSheet } from "./components/MenuSheet";
 export default function Admin() {
     return (
         <div>
-            <h1 className="text-center">Bem Vindo à Área Administrativa</h1>
-            <Button>Salvar</Button>
-            <Button variant="destructive">Excluir</Button>
-            <Button variant="secondary" >Novo</Button>
-
+            <h1 className="text-center">Área Administrativa</h1>
+        
             <MenuSheet/>
            
         </div>
